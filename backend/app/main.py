c46b8488e1fe12fc8071ae95304976e96af95e41
ryanthec <ryancheongew@gmail.com>
2026-01-03 22:17:56 +0800

    minor bug fixes for prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://167.71.217.164", "https://167.71.217.164"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
